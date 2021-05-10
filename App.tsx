5941b4dd7129508db6123cfcc18888d5070a4d7a
adriton <adrtler@gmail.com>
2021-05-10 17:19:06 +0200

    Primer commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 45,
+          textAlign: 'center',
+        }}>
+        Hola Mundo!!
+      </Text>
+    </View>
+  );
+};
+
+export default App;
