9585ab5823eb8d6deb3d71183f07e73965f8f6d7
adriton <adrtler@gmail.com>
2021-05-10 18:50:39 +0200

    TouchableOpacity y StyleSheet

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,21 +1,34 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 export const ContadorScreen = () => {
   const [contador, setContador] = useState(10);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 40,
-          top: -15,
-        }}>
-        Contador: {contador}
-      </Text>
-      <Button title="+1" onPress={() => setContador(contador + 1)} />
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Contador: {contador}</Text>
+      <TouchableOpacity onPress={() => setContador(contador + 1)}>
+        <View style={styles.botonIncrementar}>
+          <Text>+1</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15,
+  },
+  botonIncrementar: {
+    backgroundColor: 'red',
+    borderRadius: 100,
+  },
+});
