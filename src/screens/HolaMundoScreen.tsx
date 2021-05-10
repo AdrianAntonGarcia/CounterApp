9c353eea1516b1fd6487f36dc12de217dcf68841
adriton <adrtler@gmail.com>
2021-05-10 17:28:03 +0200

    Primera pantalla

---
+++ src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export const HolaMundoScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 45,
+          textAlign: 'center',
+        }}>
+        Hola Adrián
+      </Text>
+    </View>
+  );
+};
