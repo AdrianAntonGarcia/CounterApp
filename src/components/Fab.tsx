a9acd956427355f4fc327d615f6babf1c2bf8b66
adriton <adrtler@gmail.com>
2021-05-15 13:03:33 +0200

    Estilo condicional

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableNativeFeedback} from 'react-native';
 
 interface Props {
   title: string;
@@ -8,25 +8,32 @@ interface Props {
 }
 
 export const Fab = ({title, onPress, position = 'br'}: Props) => {
-  console.log(title);
   return (
-    <TouchableOpacity style={styles.fabLocationBR} onPress={onPress}>
-      <View style={styles.fab}>
-        <Text style={styles.fabText}>+1</Text>
-      </View>
-    </TouchableOpacity>
+    <View
+      style={[
+        styles.fabLocation,
+        position === 'bl' ? styles.left : styles.right,
+      ]}>
+      <TouchableNativeFeedback
+        onPress={onPress}
+        background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  fabLocationBR: {
+  fabLocation: {
     position: 'absolute',
     bottom: 25,
+  },
+  right: {
     right: 25,
   },
-  fabLocationBL: {
-    position: 'absolute',
-    bottom: 25,
+  left: {
     left: 25,
   },
   fab: {
@@ -35,6 +42,14 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 100,
     justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 2,
   },
   fabText: {
     color: 'white',
